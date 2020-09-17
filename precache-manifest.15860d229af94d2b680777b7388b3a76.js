0b6ceaf76297c6c8c9a00d17d6bd37b7ffb27fa5
monoco <mcgradytansy@gmail.com>
2020-09-16 23:00:34 -0400

    Updates

---
+++ precache-manifest.15860d229af94d2b680777b7388b3a76.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b84b2952b750c933f27b400c69685e",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "a37dbd5d19f460c70d92",
+    "url": "/web-portfolio/static/css/2.753aa48a.chunk.css"
+  },
+  {
+    "revision": "a943ea2f398af87f45d3",
+    "url": "/web-portfolio/static/css/main.37882412.chunk.css"
+  },
+  {
+    "revision": "a37dbd5d19f460c70d92",
+    "url": "/web-portfolio/static/js/2.4383f7dc.chunk.js"
+  },
+  {
+    "revision": "a943ea2f398af87f45d3",
+    "url": "/web-portfolio/static/js/main.dd905697.chunk.js"
+  },
+  {
+    "revision": "4bb87171ac4011a9a17b",
+    "url": "/web-portfolio/static/js/runtime~main.790e8c02.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/web-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/web-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/web-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/web-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
